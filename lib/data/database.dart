43ddb35533aca695b19c65b0c98a6448ff8fb86c
Gustavo Kistt <gustavokistt@hotmail.com>
2023-02-14 11:26:13 -0300

    Added task_dao.dart file to implement Dao.

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -1,23 +1,17 @@
+
+import 'package:lista_de_tarefas_app/data/task_dao.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
 Future<Database> getDataBase() async {
-  final String path = join(
-      await getDatabasesPath(), 'task.db'); //caminho do banco de dados
+  final String path =
+      join(await getDatabasesPath(), 'task.db'); //caminho do banco de dados
 
-  return openDatabase(path, onCreate: (db,
-      version) { //cria o banco de dados, caso não tenha o banco de dados, ele cria.
-    db.execute(tableSql);
+  return openDatabase(path, onCreate: (db, version) {
+    //cria o banco de dados, caso não tenha o banco de dados, ele cria.
+    db.execute(TaskDao.tableSql);
   }, version: 1);
+
+
+
 }
-
-const String tableSql = 'CREATE TABLE $_tablename('
-    '$_name TEXT, '
-    '$_difficulty INTEGER, '
-    '$_image TEXT)';
-
-
-const String _tablename = "taskTable";
-const String _name = "name";
-const String _difficulty = "difficulty";
-const String _image = "image";
